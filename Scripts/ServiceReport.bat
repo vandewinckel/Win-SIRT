b6c19b81210e1acbe8f2c4514a2970c9d6fc69c4
vandewinckel <52665545+vandewinckel@users.noreply.github.com>
2019-07-08 11:52:51 -0400

    Updated servicereport.bat

--- Scripts/ServiceReport.bat
+++ Scripts/ServiceReport.bat
@@ -4,12 +4,8 @@ echo Retrieving Process Information
 IF NOT EXIST "%Computername%" mkdir %Computername%
 cd %cd%/%Computername%/
 (
-rem PROCESSES
-wmic process list brief /format:htable
+wmic process get Caption, Processid /format:htable
 
 rem STARTUP
 wmic startup list brief /format:htable
-	
-rem SERVICES
-wmic service list brief /format:htable
 ) > ServiceReport.html
